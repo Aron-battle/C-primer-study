e194ab00f104631bb4807197933f39a61b70dbf4
Aron-battle <Jackoftao@outlook.com>
2024-09-06 23:10:24 +0800

    了解了%*d是什么意思，了解了double和float的精度
    那么（"%*d"，x）就是将X输出所占的空格位，全部换成空格
    double的最大精度为15，float的最大精度为6

---
+++ TEST_09_06.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<float.h>
+//int main(void)
+//{
+//	int num1, num2;
+//	char name1[20], name2[20];
+//	printf("请输入你的姓\n");
+//	scanf_s("%s", name1, 20);
+//	printf("请输入你的名\n");
+//	scanf_s("%s", name2, 20);
+//	 num1 = strlen(name1);
+//	 num2 = strlen(name2);
+//	printf("%s %s\n%*d %*d\n", name1, name2, num1,num1, num2,num2);
+//	printf("%s %s\n%-*d %-*d\n", name1, name2, num1,num1,num2, num2);
+//	//* 格式控制符，表示按变量 num1 和 num2 中的值进行格式化。对齐格式
+//	return 0;
+//}
+//int main(void)
+//{
+//	double m = 1.0 / 3.0;
+//	float n = 1.0 / 3.0;
+//	printf("%.6f,%.12f,%.16f\n%.6f,%.12f,%.16f\n", m, m, m,n,n,n);
+//	printf("%d %d", FLT_DIG,DBL_DIG);
+//	return 0;
+//}
